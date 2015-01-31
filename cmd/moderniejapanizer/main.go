02637c9c5d0660427a51c23845805422896667c5
Hiroaki Nakakmura <hnakamur@gmail.com>
2015-01-31 13:58:18 +0900

    Lock IE version

--- cmd/moderniejapanizer/main.go
+++ cmd/moderniejapanizer/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/hnakamur/ieversionlocker"
 	"github.com/hnakamur/moderniejapanizer"
 	"github.com/hnakamur/w32syscall"
 	"github.com/hnakamur/windowsupdate"
@@ -13,22 +15,34 @@ func main() {
 	ole.CoInitialize(0)
 	defer ole.CoUninitialize()
 
-	_, _, err := windowsupdate.InstallLanguagePack(windowsupdate.JapaneseLanguagePackUpdateID)
+	version, err := ieversionlocker.CurrentVersion()
+	if err != nil {
+		fmt.Println("Failed detect IE version: %s", err)
+		os.Exit(1)
+	}
+
+	err = ieversionlocker.Lock(version)
+	if err != nil {
+		fmt.Println("Failed to lock IE version: %s", err)
+		os.Exit(1)
+	}
+
+	_, _, err = windowsupdate.InstallLanguagePack(windowsupdate.JapaneseLanguagePackUpdateID)
 	if err != nil {
 		fmt.Printf("Error while installing Japanese Language Pack: %v\n", err)
-		return
+		os.Exit(1)
 	}
 
 	err = moderniejapanizer.SetLanguageAndRegionalFormats(moderniejapanizer.JapaneseLanguageAndRegionalFormats)
 	if err != nil {
 		fmt.Printf("Error while setting language and regional formats: %v\n", err)
-		return
+		os.Exit(1)
 	}
 
 	err = moderniejapanizer.SetLocation(moderniejapanizer.JapaneseLocationCode)
 	if err != nil {
 		fmt.Printf("Error while setting location: %v\n", err)
-		return
+		os.Exit(1)
 	}
 
 	err = moderniejapanizer.SetKeyboards([]string{
@@ -36,24 +50,24 @@ func main() {
 		moderniejapanizer.EnglishUnitedStatesKeyboardCode})
 	if err != nil {
 		fmt.Printf("Error while setting keyboards: %v\n", err)
-		return
+		os.Exit(1)
 	}
 
 	err = moderniejapanizer.SetDisplayLanguage(moderniejapanizer.JapaneseDisplayLanguageCode)
 	if err != nil {
 		fmt.Printf("Error while setting display language: %v\n", err)
-		return
+		os.Exit(1)
 	}
 
 	_, _, err = windowsupdate.InstallImportantUpdates()
 	if err != nil {
 		fmt.Printf("Error while installing important windows updates: %v\n", err)
-		return
+		os.Exit(1)
 	}
 
 	err = moderniejapanizer.Reboot(w32syscall.SHTDN_REASON_MINOR_SECURITYFIX)
 	if err != nil {
 		fmt.Printf("Error while rebooting: %v\n", err)
-		return
+		os.Exit(1)
 	}
 }
